76b7280907571bced88dd507dfb212fe30e0d17b
Amna khan <amnakhang09876@gmail.com>
2025-02-24 19:04:49 +0500

    updates

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import asyncio
 from source.loading import dataloading
-dataloading(path)
+from source.chunking import splitting
+
+BOOK = "data/A-Z Family Medical Encyclopedia.pdf"
+
+async def main():
+    data = await dataloading(BOOK)  #  Await the coroutine properly
+    splitted = splitting(data)  # Assuming `splitting` is a normal function
+    print(len(splitted))
+    
+
+asyncio.run(main())  #  Runs the async function
+
+
 
